4bf5828e2eb6c8448a53367b3dae74814bbba5a6
William-Fonseca-Geralde <127157689+William-Fonseca-Geralde@users.noreply.github.com>
2023-09-01 15:14:47 -0300

    commit

--- aulas/aula1/principal/principal.java
+++ aulas/aula1/principal/principal.java
@@ -2,15 +2,33 @@ package aulas.aula1.principal;
 
 public class principal {
   public static void main(String[] args){
+    //instanciando um objeto
     caneta c1 = new caneta();
     
+    //adicionado as características
     c1.cor = "Azul";
     c1.modelo = "Bic";
     c1.ponta = 0.5f;
     c1.tampar();
     c1.carga = 2;
 
+    //mostrando o estado desse objeto
     c1.status();
     c1.rabiscar();
+
+    //instanciando um novo objeto
+    caneta c2 = new caneta();
+
+    //adicionado as características
+    c2.cor = "Vermelho";
+    c2.modelo = "Bic";
+    c2.ponta = 0.4f;
+    c2.destampar();
+    c2.carga = 3;
+
+    //mostrando o estado desse objeto
+    c2.status();
+    c2.rabiscar();
+
   }
 }
