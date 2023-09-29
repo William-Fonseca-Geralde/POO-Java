87533c6f709db88ec74238ecd316f14be66d746d
William-Fonseca-Geralde <127157689+William-Fonseca-Geralde@users.noreply.github.com>
2023-09-29 14:49:34 -0300

    commit

--- aulas/aula9/codigo/Funcioanrio.java
+++ aulas/aula9/codigo/Funcioanrio.java
@@ -1,5 +1,24 @@
 package aulas.aula9.codigo;
 
 public class Funcioanrio extends Pessoa {
-  
+  private String setor;
+  private boolean trabalhando;
+
+  public void mudarTrabalho(){
+    setTrabalhando(!isTrabalhando());
+  }
+
+  public String getSetor() {
+    return setor;
+  }
+  public void setSetor(String setor) {
+    this.setor = setor;
+  }
+
+  public boolean isTrabalhando() {
+    return trabalhando;
+  }
+  public void setTrabalhando(boolean trabalhando) {
+    this.trabalhando = trabalhando;
+  }
 }
